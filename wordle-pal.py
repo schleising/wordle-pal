650d89e6bd4f645bbb0fbb3b5e49c032d601d317
schleising <stephen@schleising.net>
2022-02-06 15:42:49 +0000

    Update README.md with results

--- wordle-pal.py
+++ wordle-pal.py
@@ -18,3 +18,18 @@ print('Top ten first guess words')
 print('=========================')
 print()
 for count, (word, score) in enumerate(list(words.wordScoresByScore.items())[:10]): print(f'{count + 1:2}) {word.upper()} - Score: {score}')
+
+with open('README.md', 'w') as readmeFile:
+    readmeFile.write('# wordle-pal\n')
+    readmeFile.write('Help with Wordle words\n')
+    readmeFile.write('<br>\n')
+    readmeFile.write('<br>\n\n')
+    readmeFile.write('## Top 10 Starting Words\n')
+
+    for count, (word, score) in enumerate(list(words.wordScoresByScore.items())[:10]): readmeFile.write(f'    {count + 1:2}) {word.upper()} - Score: {score}\n')
+
+    readmeFile.write('<br>\n')
+    readmeFile.write('<br>\n\n')
+
+    readmeFile.write('## Today\'s Word\n')
+    readmeFile.write(f'{words.todaysWord}\n')
