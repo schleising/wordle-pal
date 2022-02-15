40f6545507820a92e2219e78abac5ed7395be2c8
schleising <stephen@schleising.net>
2022-02-15 19:27:05 +0000

    Tidy up

--- wordlepalbot.py
+++ wordlepalbot.py
@@ -1,16 +1,4 @@
-"""
-Simple Bot to reply to Telegram messages.
-
-First, a few handler functions are defined. Then, those functions are passed to
-the Dispatcher and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-
-Usage:
-Basic Echobot example, repeats messages.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
-
+import sys
 from datetime import date
 import logging
 from pathlib import Path
@@ -19,65 +7,63 @@ from telegram.ext import Updater, CommandHandler
 
 from wordlepal import GuessWord
 
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-
-logger = logging.getLogger(__name__)
-
-
-# Define a few command handlers. These usually take the two arguments update and
-# context. Error handlers also receive the raised TelegramError object in error.
-def start(update, context):
-    """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
-
-
-def help(update, context):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
-
-
+# Define a handler for /guess
 def guess(update, context):
+    # Print the date and user to the log
     print(f'{date.today()} Instigated by {update.message.from_user.first_name} {update.message.from_user.last_name}')
+
+    # Guess the word returning the day number and guess history for the response
     dayNumber, guessHistory = GuessWord(False)
+
+    # Join the guess history lines into strings
     guessStrings = [''.join(guessGraphic) for guessGraphic in guessHistory]
+
+    # Create a list for the output text
     msgLines: list[str] = []
+
+    # Add the first line of text which shows how many guesses it took
     msgLines.append(f'Wordle {dayNumber} {len(guessHistory)}/6')
+
+    # Add a blank line
     msgLines.append('')
+
+    # Add the guess history strings
     msgLines.extend(guessStrings)
 
+    # Send the reply removing the quote of the original /guess message
     update.message.reply_text('\n'.join(msgLines), quote=False)
 
+# Log errors
 def error(update, context):
-    """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
-
+# Main function
 def main():
-    """Start the bot."""
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    with open(Path('secret.txt'), 'r', encoding='utf8') as secretFile:
-        token = secretFile.read()
+    try:
+        # Get the token from the secret.txt file, this is exclued from git, so may not exist
+        with open(Path('secret.txt'), 'r', encoding='utf8') as secretFile:
+            token = secretFile.read()
+    except:
+        # If secret.txt is not available, print some help and exit
+        print('No secret.txt file found, you need to put your token from BotFather in here')
+        sys.exit()
 
+    # Create the updater
     updater = Updater(token, use_context=True)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-
-    # on noncommand i.e message - echo the message on Telegram
+    # On receipt of a /guess command call the guess() function
     dp.add_handler(CommandHandler('guess', guess))
 
-    # log all errors
+    # Add the error handler to log errors
     dp.add_error_handler(error)
 
-    # Start the Bot
+    # Start the bot polling
     updater.start_polling()
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
@@ -85,6 +71,12 @@ def main():
     # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
-
 if __name__ == '__main__':
+    # Enable logging
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
+
+    logger = logging.getLogger(__name__)
+
+    # Call the main function
     main()
