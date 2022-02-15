d0ed040a3b2c34a5533c13a2f6273e886d67d90b
schleising <stephen@schleising.net>
2022-02-15 12:46:55 +0000

    Indicate which properties should not be acessed externally

--- WordList/DownloadWords.py
+++ WordList/DownloadWords.py
@@ -8,10 +8,10 @@ from WordList.ValidWords import VALID_WORDS
 class WordDownloader():
     def __init__(self, url: str = 'https://www.nytimes.com/games/wordle/main.bd4cb59c.js') -> None:
         # Set the delimiters used to extract the words from the JavaScript
-        self.solutionWordsDelimiter = 'Ma='
-        self.validWordsDelimiter = ',Oa='
-        self.endValidWordsDelimiter = ',Ra='
-        self.url = url
+        self._solutionWordsDelimiter = 'Ma='
+        self._validWordsDelimiter = ',Oa='
+        self._endValidWordsDelimiter = ',Ra='
+        self._url = url
 
         # If the JavaScript did not download correctly, use the defaults
         self.solutionWords = SOLUTION_WORDS
@@ -22,7 +22,7 @@ class WordDownloader():
         
     def _DownloadWords(self) -> None:
         # Get the JavaScript file
-        response = requests.get(self.url)
+        response = requests.get(self._url)
 
         # Check the response code, if OK, update the defaults with the downloaded words
         if response.status_code == requests.codes.OK:
@@ -31,8 +31,8 @@ class WordDownloader():
 
             try:
                 # Extract the text using the delimiters and load the JSON string into a Python list
-                self.solutionWords: list[str] = json.loads(fullText.split(self.solutionWordsDelimiter)[1].split(self.validWordsDelimiter)[0])
-                self.validWords: list[str] = json.loads(fullText.split(self.validWordsDelimiter)[1].split(self.endValidWordsDelimiter)[0])
+                self.solutionWords: list[str] = json.loads(fullText.split(self._solutionWordsDelimiter)[1].split(self._validWordsDelimiter)[0])
+                self.validWords: list[str] = json.loads(fullText.split(self._validWordsDelimiter)[1].split(self._endValidWordsDelimiter)[0])
             except:
                 # If there is any kind of parsing error, reset the words back to the original ones
                 self.solutionWords = SOLUTION_WORDS
