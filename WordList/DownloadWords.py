0b8205ae90ff7312dfe5dbb793def37a4a7f06ba
schleising <stephen@schleising.net>
2022-02-17 12:37:03 +0000

    Move try, except, else block to handle download errors and well as parsing errors

--- WordList/DownloadWords.py
+++ WordList/DownloadWords.py
@@ -22,32 +22,32 @@ class WordDownloader():
             self._DownloadWords()
         
     def _DownloadWords(self) -> None:
-        # Get the JavaScript file
-        response = requests.get(self._url)
+        try:
+            # Get the JavaScript file
+            response = requests.get(self._url)
 
-        # Check the response code, if OK, update the defaults with the downloaded words
-        if response.status_code == requests.codes.OK:
-            # If the download was OK, get the full text
-            fullText = response.text
+            # Check the response code, if OK, update the defaults with the downloaded words
+            if response.status_code == requests.codes.OK:
+                # If the download was OK, get the full text
+                fullText = response.text
 
-            try:
                 # Extract the text using the delimiters and load the JSON string into a Python list
                 self.solutionWords: list[str] = json.loads(fullText.split(self._solutionWordsDelimiter)[1].split(self._validWordsDelimiter)[0])
                 self.validWords: list[str] = json.loads(fullText.split(self._validWordsDelimiter)[1].split(self._endValidWordsDelimiter)[0])
-            except:
-                # If there is any kind of parsing error, reset the words back to the original ones
-                self.solutionWords = SOLUTION_WORDS
-                self.validWords = VALID_WORDS
-            else:
-                # If parsing was successful, update the default solution and
-                # valid word files in case of changes for use another day if necessary
-                with open(Path('WordList/SolutionWords.py'), 'w', encoding='utf-8') as solutionFile:
-                    wordList = json.dumps(self.solutionWords)
-                    solutionFile.write(f'SOLUTION_WORDS: list[str] = {wordList}')
+        except:
+            # If there is any kind of download or parsing error, reset the words back to the original ones
+            self.solutionWords = SOLUTION_WORDS
+            self.validWords = VALID_WORDS
+        else:
+            # If download and parsing was successful, update the default solution and
+            # valid word files in case of changes for use another day if necessary
+            with open(Path('WordList/SolutionWords.py'), 'w', encoding='utf-8') as solutionFile:
+                wordList = json.dumps(self.solutionWords)
+                solutionFile.write(f'SOLUTION_WORDS: list[str] = {wordList}')
 
-                with open(Path('WordList/ValidWords.py'), 'w', encoding='utf-8') as validFile:
-                    wordList = json.dumps(self.validWords)
-                    validFile.write(f'VALID_WORDS: list[str] = {wordList}')
+            with open(Path('WordList/ValidWords.py'), 'w', encoding='utf-8') as validFile:
+                wordList = json.dumps(self.validWords)
+                validFile.write(f'VALID_WORDS: list[str] = {wordList}')
 
 if __name__ == '__main__':
     # Test this class
